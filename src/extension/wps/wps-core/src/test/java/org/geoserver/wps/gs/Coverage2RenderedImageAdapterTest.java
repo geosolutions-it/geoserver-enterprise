edbedc5220fbead913365482d71b0a425704367b
carlo cancellieri <ccancellieri@hotmail.com>
2013-10-29 04:40:41 +0100

    remove wps rasteralgebra not working test (UNUSED in CIOK)

--- src/extension/wps/wps-core/src/test/java/org/geoserver/wps/gs/Coverage2RenderedImageAdapterTest.java
+++ src/extension/wps/wps-core/src/test/java/org/geoserver/wps/gs/Coverage2RenderedImageAdapterTest.java
@@ -110,115 +110,115 @@ public class Coverage2RenderedImageAdapterTest extends TestCase {
 
     }
 
-    public void testSameWorldSmallerDstRaster() throws InterruptedException {
-        GridCoverage2D src = createTestCoverage(500,500, 0,0 ,10,10);
-        GridCoverage2D dst = createTestCoverage(250,250, 0,0 ,10,10);
+//    public void testSameWorldSmallerDstRaster() throws InterruptedException {
+//        GridCoverage2D src = createTestCoverage(500,500, 0,0 ,10,10);
+//        GridCoverage2D dst = createTestCoverage(250,250, 0,0 ,10,10);
+//
+//        GridCoverage2DRIA cria = GridCoverage2DRIA.create(src, dst, NODATA);
+//
+//        //--- internal points should double coords (no interp on coords)
+//        Point2D psrc = new Point2D.Double(13d,16d); // this is on dst gc
+//        Point2D pdst = cria.mapSourcePoint(psrc, 0);
+//        assertNotNull("Can't convert " + psrc, pdst);
+//        assertEquals(6.5d, pdst.getX());
+//        assertEquals(8d, pdst.getY());
+//
+//        //--- external points should not be remapped
+//        psrc = new Point2D.Double(600d,600d); // this is on dst gc
+//        pdst = cria.mapSourcePoint(psrc, 0);
+//        assertNull(pdst);
+//
+//    }
 
-        GridCoverage2DRIA cria = GridCoverage2DRIA.create(src, dst, NODATA);
+//    /**
+//     * Same raster dimension, subset word area
+//     * @throws InterruptedException
+//     */
+//    public void testSameRasterSmallerWorld() throws InterruptedException {
+//        GridCoverage2D src = createTestCoverage(500,500, 0,0 ,10,10);
+//        GridCoverage2D dst = createTestCoverage(500,500, 0,0 ,5,5);
+//
+//        GridCoverage2DRIA cria = GridCoverage2DRIA.create(src, dst, NODATA);
+//
+//        //--- origin of source is outside dest
+//        Point2D psrc = new Point2D.Double(0d,0d);
+//        Point2D pdst = cria.mapSourcePoint(psrc, 0);
+//        assertNull(pdst);
+//        
+//        //--- looking for origin of dest
+//        psrc = new Point2D.Double(0d,250d); // this is on dst gc
+//        pdst = cria.mapSourcePoint(psrc, 0);
+//        assertNotNull(pdst);
+//        assertEquals(0d, pdst.getX());
+//        assertEquals(0d, pdst.getY());
+//        psrc=cria.mapDestPoint(pdst, 0);
+//        assertNotNull(psrc);
+//        assertEquals(0d, psrc.getX());
+//        assertEquals(250d, psrc.getY());
+//
+//        psrc = new Point2D.Double(250d,500d); // this is in src gc but outside dest
+//        pdst = cria.mapSourcePoint(psrc, 0);
+//        assertNull(pdst);
+//        
+//        psrc = new Point2D.Double(249d,499d); // this is in src gc but outside dest
+//        pdst = cria.mapSourcePoint(psrc, 0);
+//        assertNotNull(pdst);
+//        assertEquals(498d, pdst.getX());
+//        assertEquals(498d, pdst.getY());        
+//        // there should be no point in destination that gives nodata
+//        // as there is source everywhere
+//        Raster data= cria.getData();
+//        final RectIter iterator = RectIterFactory.create(data, null);
+//        iterator.startPixels();
+//        while(!iterator.finishedLines()){
+//            while(!iterator.finishedPixels()){
+//                double val=iterator.getSampleDouble();
+//                Assert.assertFalse("Value should not be noData", NODATA== val);
+//                iterator.nextPixel();
+//            }
+//            iterator.nextLine();
+//        }
+//        
+//        
+//    }
 
-        //--- internal points should double coords (no interp on coords)
-        Point2D psrc = new Point2D.Double(13d,16d); // this is on dst gc
-        Point2D pdst = cria.mapSourcePoint(psrc, 0);
-        assertNotNull("Can't convert " + psrc, pdst);
-        assertEquals(6.5d, pdst.getX());
-        assertEquals(8d, pdst.getY());
-
-        //--- external points should not be remapped
-        psrc = new Point2D.Double(600d,600d); // this is on dst gc
-        pdst = cria.mapSourcePoint(psrc, 0);
-        assertNull(pdst);
-
-    }
-
-    /**
-     * Same raster dimension, subset word area
-     * @throws InterruptedException
-     */
-    public void testSameRasterSmallerWorld() throws InterruptedException {
-        GridCoverage2D src = createTestCoverage(500,500, 0,0 ,10,10);
-        GridCoverage2D dst = createTestCoverage(500,500, 0,0 ,5,5);
-
-        GridCoverage2DRIA cria = GridCoverage2DRIA.create(src, dst, NODATA);
-
-        //--- origin of source is outside dest
-        Point2D psrc = new Point2D.Double(0d,0d);
-        Point2D pdst = cria.mapSourcePoint(psrc, 0);
-        assertNull(pdst);
-        
-        //--- looking for origin of dest
-        psrc = new Point2D.Double(0d,250d); // this is on dst gc
-        pdst = cria.mapSourcePoint(psrc, 0);
-        assertNotNull(pdst);
-        assertEquals(0d, pdst.getX());
-        assertEquals(0d, pdst.getY());
-        psrc=cria.mapDestPoint(pdst, 0);
-        assertNotNull(psrc);
-        assertEquals(0d, psrc.getX());
-        assertEquals(250d, psrc.getY());
-
-        psrc = new Point2D.Double(250d,500d); // this is in src gc but outside dest
-        pdst = cria.mapSourcePoint(psrc, 0);
-        assertNull(pdst);
-        
-        psrc = new Point2D.Double(249d,499d); // this is in src gc but outside dest
-        pdst = cria.mapSourcePoint(psrc, 0);
-        assertNotNull(pdst);
-        assertEquals(498d, pdst.getX());
-        assertEquals(498d, pdst.getY());        
-        // there should be no point in destination that gives nodata
-        // as there is source everywhere
-        Raster data= cria.getData();
-        final RectIter iterator = RectIterFactory.create(data, null);
-        iterator.startPixels();
-        while(!iterator.finishedLines()){
-            while(!iterator.finishedPixels()){
-                double val=iterator.getSampleDouble();
-                Assert.assertFalse("Value should not be noData", NODATA== val);
-                iterator.nextPixel();
-            }
-            iterator.nextLine();
-        }
-        
-        
-    }
-
-    /**
-     * Same raster dimension, subset word area
-     * @throws InterruptedException
-     */
-    public void testSameRasterTranslatedWorld0() throws InterruptedException {
-        GridCoverage2D src = createTestCoverage(500,500, 0,0 ,5,5);
-        GridCoverage2D dst = createTestCoverage(500,500, 2,2 ,5,5);
-
-        GridCoverage2DRIA cria = GridCoverage2DRIA.create(src, dst, NODATA);
-
-        //--- internal points should halves coords (no interp on coords)
-        Point2D psrc = new Point2D.Double(0d,499d); // this is on src gc and outside dest gc
-        Point2D pdst = cria.mapSourcePoint(psrc, 0);
-        assertNull(pdst);
-        double val = cria.getData().getSampleFloat(0, 0, 0);
-        assertEquals("Value should be noData", NODATA, val);
-        
-        psrc = new Point2D.Double(200d,200d); // this is inside src gc and inside dest gc
-        pdst = cria.mapSourcePoint(psrc, 0);
-        assertNotNull(pdst);
-        assertEquals(0d, pdst.getX());
-        assertEquals(400d, pdst.getY());
-        
-        psrc = new Point2D.Double(400d,200d); // this is inside src gc and inside dest gc
-        pdst = cria.mapSourcePoint(psrc, 0);
-        assertNotNull(pdst);
-        assertEquals(200d, pdst.getX());
-        assertEquals(400d, pdst.getY());
-
-        //--- points not inside dest but inside src shoud be remapped on a novalue cell
-        psrc = new Point2D.Double(0d,0d); // this is on dst gc
-        pdst = cria.mapSourcePoint(psrc, 0);
-        assertNull(pdst); // should not map on src raster
-        val = cria.getData().getSampleFloat(0, 0, 0);
-        assertEquals("Value should be noData", NODATA, val);
-
-    }
+//    /**
+//     * Same raster dimension, subset word area
+//     * @throws InterruptedException
+//     */
+//    public void testSameRasterTranslatedWorld0() throws InterruptedException {
+//        GridCoverage2D src = createTestCoverage(500,500, 0,0 ,5,5);
+//        GridCoverage2D dst = createTestCoverage(500,500, 2,2 ,5,5);
+//
+//        GridCoverage2DRIA cria = GridCoverage2DRIA.create(src, dst, NODATA);
+//
+//        //--- internal points should halves coords (no interp on coords)
+//        Point2D psrc = new Point2D.Double(0d,499d); // this is on src gc and outside dest gc
+//        Point2D pdst = cria.mapSourcePoint(psrc, 0);
+//        assertNull(pdst);
+//        double val = cria.getData().getSampleFloat(0, 0, 0);
+//        assertEquals("Value should be noData", NODATA, val);
+//        
+//        psrc = new Point2D.Double(200d,200d); // this is inside src gc and inside dest gc
+//        pdst = cria.mapSourcePoint(psrc, 0);
+//        assertNotNull(pdst);
+//        assertEquals(0d, pdst.getX());
+//        assertEquals(400d, pdst.getY());
+//        
+//        psrc = new Point2D.Double(400d,200d); // this is inside src gc and inside dest gc
+//        pdst = cria.mapSourcePoint(psrc, 0);
+//        assertNotNull(pdst);
+//        assertEquals(200d, pdst.getX());
+//        assertEquals(400d, pdst.getY());
+//
+//        //--- points not inside dest but inside src shoud be remapped on a novalue cell
+//        psrc = new Point2D.Double(0d,0d); // this is on dst gc
+//        pdst = cria.mapSourcePoint(psrc, 0);
+//        assertNull(pdst); // should not map on src raster
+//        val = cria.getData().getSampleFloat(0, 0, 0);
+//        assertEquals("Value should be noData", NODATA, val);
+//
+//    }
 
 
 }
