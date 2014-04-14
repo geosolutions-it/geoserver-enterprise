896a3684685bda856aad4837f0c66903ea8b6d8f
carlo <ccancellieri@hotmail.com>
2014-04-14 19:51:56 +0200

    [cluster] move properties into cluster config dir

---
+++ src/community/cluster/jms-activeMQ/src/main/java/it/geosolutions/geoserver/jms/impl/JMSXBeanBrokerFactory.java
@@ -0,0 +1,38 @@
+package it.geosolutions.geoserver.jms.impl;
+
+import java.net.MalformedURLException;
+
+import org.apache.activemq.spring.Utils;
+import org.apache.activemq.xbean.XBeanBrokerFactory;
+import org.apache.xbean.spring.context.ResourceXmlApplicationContext;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.FatalBeanException;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.core.io.Resource;
+
+public class JMSXBeanBrokerFactory extends XBeanBrokerFactory implements ApplicationContextAware {
+	
+	private ApplicationContext context;
+
+	protected ApplicationContext createApplicationContext(String uri) throws MalformedURLException {
+        Resource resource = Utils.resourceFromString(uri);
+        try {
+            return new ResourceXmlApplicationContext(resource,context) {
+                @Override
+                protected void initBeanDefinitionReader(XmlBeanDefinitionReader reader) {
+                    reader.setValidating(isValidate());
+                }
+            };
+        } catch (FatalBeanException errorToLog) {
+            throw errorToLog;
+        }
+    }
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext)
+			throws BeansException {
+		this.context=applicationContext;
+	}
+}
