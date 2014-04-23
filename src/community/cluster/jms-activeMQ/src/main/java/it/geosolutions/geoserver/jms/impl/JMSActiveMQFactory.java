4aa402d0ab858978f421336cf564c30439d15ec9
carlo <ccancellieri@hotmail.com>
2014-04-23 18:15:19 +0200

    [cluster] fix brokerURI default settings

--- src/community/cluster/jms-activeMQ/src/main/java/it/geosolutions/geoserver/jms/impl/JMSActiveMQFactory.java
+++ src/community/cluster/jms-activeMQ/src/main/java/it/geosolutions/geoserver/jms/impl/JMSActiveMQFactory.java
@@ -14,6 +14,7 @@ import it.geosolutions.geoserver.jms.configuration.TopicConfiguration;
 
 import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Properties;
 import java.util.logging.Level;
 
@@ -24,7 +25,6 @@ import javax.jms.Topic;
 
 import org.apache.activemq.broker.BrokerService;
 import org.apache.activemq.pool.PooledConnectionFactory;
-import org.apache.activemq.xbean.XBeanBrokerFactory;
 import org.geotools.util.logging.Logging;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,259 +36,320 @@ import org.springframework.beans.factory.annotation.Autowired;
  */
 public class JMSActiveMQFactory extends JMSFactory implements InitializingBean {
 
-    private final static java.util.logging.Logger LOGGER = Logging
-            .getLogger(JMSActiveMQFactory.class);
+	private final static java.util.logging.Logger LOGGER = Logging
+			.getLogger(JMSActiveMQFactory.class);
 
-    @Autowired
-    private JMSConfiguration config;
+	@Autowired
+	private JMSConfiguration config;
 
-    // used to track changes to the configuration
-    private String brokerURI;
+	@Autowired
+	private JMSXBeanBrokerFactory bf;
 
-    private String topicName;
+	// used to track changes to the configuration
+	private String brokerURI;
 
-    private PooledConnectionFactory cf;
+	private String topicName;
 
-    private Topic topic;
+	private PooledConnectionFactory cf;
 
-    // times to test (connection)
-    private static int max;
+	private Topic topic;
 
-    // millisecs to wait between tests (connection)
-    private static long maxWait;
+	// times to test (connection)
+	private static int max;
 
-    // embedded brokerURI
-    private BrokerService brokerService;
+	// millisecs to wait between tests (connection)
+	private static long maxWait;
 
-    private String brokerName;
-    
-    @Override
-    public void afterPropertiesSet() throws Exception {
-    	// TODO initialize connections in order (first broker, then client)
-    }
+	// embedded brokerURI
+	private BrokerService brokerService;
 
-    // <bean id="JMSClientDestination"
-    // class="org.apache.activemq.command.ActiveMQQueue">
-    // <value="Consumer.${instance.name}.VirtualTopic.${topic.name}" />
-    // </bean>
-    @Override
-    public Destination getClientDestination(Properties configuration) {
-        StringBuilder builder = new StringBuilder("Consumer.");
-        String instanceName = configuration.getProperty(JMSConfiguration.INSTANCE_NAME_KEY);
-        String topicName = configuration.getProperty(TopicConfiguration.TOPIC_NAME_KEY);
-        return new org.apache.activemq.command.ActiveMQQueue(builder.append(instanceName)
-                .append(".").append(topicName).toString());
-    }
+	private String brokerName;
 
-    // <!-- DESTINATION -->
-    // <!-- A Destination in ActiveMQ -->
-    // <bean id="JMSServerDestination"
-    // class="org.apache.activemq.command.ActiveMQTopic">
-    // <!-- <constructor-arg value="VirtualTopic.${topic.name}" /> -->
-    // <constructor-arg value="VirtualTopic.>" />
-    // </bean>
-    @Override
-    public Topic getTopic(Properties configuration) {
-        // TODO move me to implementation jmsFactory implementation
-        // if topicName is changed
-        final String topicConfiguredName = configuration
-                .getProperty(TopicConfiguration.TOPIC_NAME_KEY);
-        if (topic == null || topicName.equals(topicConfiguredName)) {
-            topicName = topicConfiguredName;
-            topic = new org.apache.activemq.command.ActiveMQTopic(
-                    configuration.getProperty(TopicConfiguration.TOPIC_NAME_KEY));
-        }
-        if (topic == null) {
-            throw new IllegalStateException("Unable to load a JMS Topic destination");
-        }
-        return topic;
-    }
+	@Override
+	public void afterPropertiesSet() throws Exception {
 
-    // <!-- A connection to ActiveMQ -->
-    // <bean id="JMSConnectionFactory"
-    // class="org.apache.activemq.ActiveMQConnectionFactory">
-    // <!-- <property name="brokerURL" value="${brokerURI.url}" /> -->
-    // <property name="brokerURL" value="tcp://localhost:61616" />
-    // </bean>
-    @Override
-    public ConnectionFactory getConnectionFactory(Properties configuration) {
-        String _brokerURI = configuration.getProperty(BrokerConfiguration.BROKER_URL_KEY);
-        if(isEmbeddedBrokerStarted()&&(_brokerURI==null||_brokerURI.length()==0)){
-        	brokerURI="vm://"+brokerName+"?create=false&waitForStart=5000";
-        }
-        
-        if (cf == null) {
-            // need to be initialized
-            cf = new PooledConnectionFactory(brokerURI);
+		// TODO initialize connections in order (first broker, then client)
 
-        } else if (brokerURI == null || !brokerURI.equals(_brokerURI)) {
-            // clear pending connections
-            try {
-                destroy();
-            } catch (Exception e) {
-                // eat
-            }
-            // create a new connection
-            cf = new PooledConnectionFactory(brokerURI);
-            // cf.start();
-        }
-        return cf;
-    }
+	}
 
-    @Override
-    public void destroy() throws Exception {
-        if (cf != null) {
-            // close all the connections
-            cf.clear();
-            // stop the factory
-            cf.stop();
-            // set null
-            cf = null;
-        }
-        if (brokerService != null) {
-            brokerService.stop();
-        }
-    }
-    
-    @Autowired
-    JMSXBeanBrokerFactory bf;
+	// <bean id="JMSClientDestination"
+	// class="org.apache.activemq.command.ActiveMQQueue">
+	// <value="Consumer.${instance.name}.VirtualTopic.${topic.name}" />
+	// </bean>
+	@Override
+	public Destination getClientDestination(Properties configuration) {
+		StringBuilder builder = new StringBuilder("Consumer.");
+		String instanceName = configuration
+				.getProperty(JMSConfiguration.INSTANCE_NAME_KEY);
+		String topicName = configuration
+				.getProperty(TopicConfiguration.TOPIC_NAME_KEY);
+		return new org.apache.activemq.command.ActiveMQQueue(builder
+				.append(instanceName).append(".").append(topicName).toString());
+	}
 
-    @Override
-    public boolean startEmbeddedBroker(final Properties configuration) throws Exception {
-        if (LOGGER.isLoggable(Level.INFO)) {
-            LOGGER.info("Starting the embedded brokerURI");
-        }
-        if (brokerService == null) {
-            final String xBeanBroker = configuration.getProperty(ActiveMQEmbeddedBrokerConfiguration.BROKER_URL_KEY);
-//            final XBeanBrokerFactory bf = new XBeanBrokerFactory();
-            brokerService = bf.createBroker(new URI(xBeanBroker));
-            brokerService.setEnableStatistics(false);
-            
-            // override the name of the brokerURI using the instance name which should be unique within the network
-            brokerName = configuration.getProperty(JMSConfiguration.INSTANCE_NAME_KEY);
-            brokerService.setBrokerName(brokerName);
-        } else {
-            if (LOGGER.isLoggable(Level.WARNING)) {
-                LOGGER.warning("The embedded brokerURI service already exists, probably it is already started");
-            }
-            if (brokerService.isStarted()) {    
-                if (LOGGER.isLoggable(Level.WARNING)) {
-                    LOGGER.warning("SKIPPING: The embedded brokerURI is already started");
-                }
-                return true;
-            }
-        }
-        if (!brokerService.isStarted()) {
-            brokerService.start();
-        }
-        for (int i = -1; i < max; ++i) {
-            try {
-                if (brokerService.isStarted()) {
-                    if (LOGGER.isLoggable(Level.INFO)) {
-                        LOGGER.info("Embedded brokerURI is now started");
-                    }
-                    return true;
-                }
-                Thread.sleep(maxWait);
-            } catch (Exception e1) {
-                if (LOGGER.isLoggable(Level.SEVERE)) {
-                    LOGGER.severe("Unable to start the embedded brokerURI" + e1.getLocalizedMessage());
-                }
-                return false;
-            }
-        }
-        if (LOGGER.isLoggable(Level.SEVERE)) {
-            LOGGER.severe("Unable to start the embedded brokerURI");
-        }
-        return false;
-    }
+	// <!-- DESTINATION -->
+	// <!-- A Destination in ActiveMQ -->
+	// <bean id="JMSServerDestination"
+	// class="org.apache.activemq.command.ActiveMQTopic">
+	// <!-- <constructor-arg value="VirtualTopic.${topic.name}" /> -->
+	// <constructor-arg value="VirtualTopic.>" />
+	// </bean>
+	@Override
+	public Topic getTopic(Properties configuration) {
+		// TODO move me to implementation jmsFactory implementation
+		// if topicName is changed
+		final String topicConfiguredName = configuration
+				.getProperty(TopicConfiguration.TOPIC_NAME_KEY);
+		if (topic == null || topicName.equals(topicConfiguredName)) {
+			topicName = topicConfiguredName;
+			topic = new org.apache.activemq.command.ActiveMQTopic(
+					configuration
+							.getProperty(TopicConfiguration.TOPIC_NAME_KEY));
+		}
+		if (topic == null) {
+			throw new IllegalStateException(
+					"Unable to load a JMS Topic destination");
+		}
+		return topic;
+	}
 
-    @Override
-    public boolean isEmbeddedBrokerStarted() {
-        return brokerService == null ? false : brokerService.isStarted();
-    }
+	// <!-- A connection to ActiveMQ -->
+	@Override
+	public ConnectionFactory getConnectionFactory(Properties configuration) {
+		final String _brokerURI = config
+				.getConfiguration(BrokerConfiguration.BROKER_URL_KEY);
 
-    @Override
-    public boolean stopEmbeddedBroker() throws Exception {
-        if (LOGGER.isLoggable(Level.INFO)) {
-            LOGGER.info("Embedded brokerURI is now stopped");
-        }
-        if (brokerService == null) {
-            return true;
-        }
-        brokerService.stop();
-        for (int i = -1; i < max; ++i) {
-            try {
-                if (!brokerService.isStarted()) {
-                    if (LOGGER.isLoggable(Level.INFO)) {
-                        LOGGER.info("Embedded brokerURI is now stopped");
-                    }
-                    brokerService = null;
-                    return true;
-                }
-                if (LOGGER.isLoggable(Level.INFO)) {
-                    LOGGER.info("Embedded brokerURI is going to stop: waiting...");
-                }
-                Thread.sleep(maxWait);
-            } catch (Exception e1) {
-                if (LOGGER.isLoggable(Level.SEVERE)) {
-                    LOGGER.severe("Unable to start the embedded brokerURI" + e1.getLocalizedMessage());
-                }
-                return false;
-            }
-        }
-        if (LOGGER.isLoggable(Level.SEVERE)) {
-            LOGGER.severe("Unable to stop the embedded brokerURI");
-        }
-        return false;
-    }
+		final boolean changed = checkBrokerURI(_brokerURI);
 
-    @SuppressWarnings("unused")
-    @PostConstruct
-    private void init() {
-        // // times to test (connection)
-        max = Integer.parseInt(config
-                .getConfiguration(ConnectionConfiguration.CONNECTION_RETRY_KEY).toString());
-        // millisecs to wait between tests (connection)
-        maxWait = Long.parseLong(config.getConfiguration(
-                ConnectionConfiguration.CONNECTION_MAXWAIT_KEY).toString());
+		if (cf == null) {
+			// need to be initialized
+			cf = new PooledConnectionFactory(brokerURI);
 
-        // check configuration for connection and try to start if needed
-        if (EmbeddedBrokerConfiguration.isEnabled(config)) {
-            if (!isEmbeddedBrokerStarted()) {
-                try {
-                    if (!startEmbeddedBroker(config.getConfigurations())) {
-                        if (LOGGER.isLoggable(Level.SEVERE)) {
-                            LOGGER.severe("Unable to start the embedded brokerURI, force status to disabled");
-                        }
+		} else if (changed) {
+			// clear pending connections
+			try {
+				destroy();
+			} catch (Exception e) {
+				// eat
+			}
+			// create a new connection
+			cf = new PooledConnectionFactory(brokerURI);
+			// cf.start();
+		}
 
-                        // change configuration status
-                        config.putConfiguration(ConnectionConfiguration.CONNECTION_KEY,
-                                ConnectionConfigurationStatus.disabled.toString());
+		return cf;
+	}
 
-                    }else{
-                        if (LOGGER.isLoggable(Level.SEVERE)) {
-                            LOGGER.severe("Started the embedded brokerURI: "+brokerService.toString());
-                        }                        
-                    }
-                } catch (Exception e) {
-                    LOGGER.log(Level.SEVERE, e.getMessage(), e);
-                    // change configuration status
-                    config.putConfiguration(ConnectionConfiguration.CONNECTION_KEY,
-                            ConnectionConfigurationStatus.disabled.toString());
-                }
-                // store changes to the configuration
-                try {
-                    config.storeConfig();
-                } catch (IOException e) {
-                    LOGGER.log(Level.SEVERE, e.getMessage(), e);
-                }
-            } else {
-                if (LOGGER.isLoggable(Level.WARNING)) {
-                    LOGGER.warning("The brokerURI seems to be already started");
-                }
-            }
-        }
-    }
+	/**
+	 * @return true if brokerURI is modified
+	 */
+	private boolean checkBrokerURI(final String _brokerURI) {
+		if (brokerURI == null) {
+			if (_brokerURI == null || _brokerURI.length() == 0) {
+				setDefaultURI();
+				return true;
+			} else {
+				// initialize to the new configuration
+				brokerURI = _brokerURI;
+
+				// check the URI syntax
+				try {
+					new URI(brokerURI);
+				} catch (URISyntaxException e) {
+					if (LOGGER.isLoggable(Level.SEVERE)) {
+						LOGGER.severe(e.getLocalizedMessage());
+					}
+					setDefaultURI();
+				}
+				return true;
+			}
+		} else {
+			if (_brokerURI == null || _brokerURI.length() == 0) {
+				// reset to default
+				setDefaultURI();
+				return true;
+			} else if (brokerURI.equalsIgnoreCase(_brokerURI)) {
+				return false;
+			} else {
+				// something is changed: reset
+				brokerURI = null;
+				return checkBrokerURI(_brokerURI);
+			}
+		}
+	}
+
+	private void setDefaultURI() {
+		// reset default brokerURI
+		brokerURI = "vm://"
+				+ config.getConfiguration(JMSConfiguration.INSTANCE_NAME_KEY)
+				+ "?create=false&waitForStart=5000";
+		if (LOGGER.isLoggable(Level.WARNING)) {
+			LOGGER.warning("Setting brokerURI to: " + brokerURI);
+		}
+
+	}
+
+	@Override
+	public void destroy() throws Exception {
+		if (cf != null) {
+			// close all the connections
+			cf.clear();
+			// stop the factory
+			cf.stop();
+			// set null
+			cf = null;
+		}
+		if (brokerService != null) {
+			brokerService.stop();
+		}
+	}
+
+	@Override
+	public boolean startEmbeddedBroker(final Properties configuration)
+			throws Exception {
+		if (LOGGER.isLoggable(Level.INFO)) {
+			LOGGER.info("Starting the embedded brokerURI");
+		}
+		if (brokerService == null) {
+			final String xBeanBroker = configuration
+					.getProperty(ActiveMQEmbeddedBrokerConfiguration.BROKER_URL_KEY);
+			// final XBeanBrokerFactory bf = new XBeanBrokerFactory();
+			brokerService = bf.createBroker(new URI(xBeanBroker));
+			brokerService.setEnableStatistics(false);
+
+			// override the name of the brokerURI using the instance name which
+			// should be unique within the network
+			brokerName = configuration
+					.getProperty(JMSConfiguration.INSTANCE_NAME_KEY);
+			brokerService.setBrokerName(brokerName);
+		} else {
+			if (LOGGER.isLoggable(Level.WARNING)) {
+				LOGGER.warning("The embedded brokerURI service already exists, probably it is already started");
+			}
+			if (brokerService.isStarted()) {
+				if (LOGGER.isLoggable(Level.WARNING)) {
+					LOGGER.warning("SKIPPING: The embedded brokerURI is already started");
+				}
+				return true;
+			}
+		}
+		if (!brokerService.isStarted()) {
+			brokerService.start();
+		}
+		for (int i = -1; i < max; ++i) {
+			try {
+				if (brokerService.isStarted()) {
+					if (LOGGER.isLoggable(Level.INFO)) {
+						LOGGER.info("Embedded brokerURI is now started");
+					}
+					return true;
+				}
+				Thread.sleep(maxWait);
+			} catch (Exception e1) {
+				if (LOGGER.isLoggable(Level.SEVERE)) {
+					LOGGER.severe("Unable to start the embedded brokerURI"
+							+ e1.getLocalizedMessage());
+				}
+				return false;
+			}
+		}
+		if (LOGGER.isLoggable(Level.SEVERE)) {
+			LOGGER.severe("Unable to start the embedded brokerURI");
+		}
+		return false;
+	}
+
+	@Override
+	public boolean isEmbeddedBrokerStarted() {
+		return brokerService == null ? false : brokerService.isStarted();
+	}
+
+	@Override
+	public boolean stopEmbeddedBroker() throws Exception {
+		if (LOGGER.isLoggable(Level.INFO)) {
+			LOGGER.info("Embedded brokerURI is now stopped");
+		}
+		if (brokerService == null) {
+			return true;
+		}
+		brokerService.stop();
+		for (int i = -1; i < max; ++i) {
+			try {
+				if (!brokerService.isStarted()) {
+					if (LOGGER.isLoggable(Level.INFO)) {
+						LOGGER.info("Embedded brokerURI is now stopped");
+					}
+					brokerService = null;
+					return true;
+				}
+				if (LOGGER.isLoggable(Level.INFO)) {
+					LOGGER.info("Embedded brokerURI is going to stop: waiting...");
+				}
+				Thread.sleep(maxWait);
+			} catch (Exception e1) {
+				if (LOGGER.isLoggable(Level.SEVERE)) {
+					LOGGER.severe("Unable to start the embedded brokerURI"
+							+ e1.getLocalizedMessage());
+				}
+				return false;
+			}
+		}
+		if (LOGGER.isLoggable(Level.SEVERE)) {
+			LOGGER.severe("Unable to stop the embedded brokerURI");
+		}
+		return false;
+	}
+
+	@SuppressWarnings("unused")
+	@PostConstruct
+	private void init() {
+		// // times to test (connection)
+		max = Integer.parseInt(config.getConfiguration(
+				ConnectionConfiguration.CONNECTION_RETRY_KEY).toString());
+		// millisecs to wait between tests (connection)
+		maxWait = Long.parseLong(config.getConfiguration(
+				ConnectionConfiguration.CONNECTION_MAXWAIT_KEY).toString());
+
+		// check configuration for connection and try to start if needed
+		if (EmbeddedBrokerConfiguration.isEnabled(config)) {
+			if (!isEmbeddedBrokerStarted()) {
+				try {
+					if (!startEmbeddedBroker(config.getConfigurations())) {
+						if (LOGGER.isLoggable(Level.SEVERE)) {
+							LOGGER.severe("Unable to start the embedded brokerURI, force status to disabled");
+						}
+
+						// change configuration status
+						config.putConfiguration(
+								ConnectionConfiguration.CONNECTION_KEY,
+								ConnectionConfigurationStatus.disabled
+										.toString());
+
+					} else {
+						if (LOGGER.isLoggable(Level.SEVERE)) {
+							LOGGER.severe("Started the embedded brokerURI: "
+									+ brokerService.toString());
+						}
+					}
+				} catch (Exception e) {
+					LOGGER.log(Level.SEVERE, e.getMessage(), e);
+					// change configuration status
+					config.putConfiguration(
+							ConnectionConfiguration.CONNECTION_KEY,
+							ConnectionConfigurationStatus.disabled.toString());
+				}
+				// store changes to the configuration
+				try {
+					config.storeConfig();
+				} catch (IOException e) {
+					LOGGER.log(Level.SEVERE, e.getMessage(), e);
+				}
+			} else {
+				if (LOGGER.isLoggable(Level.WARNING)) {
+					LOGGER.warning("The brokerURI seems to be already started");
+				}
+			}
+		}
+	}
 
 }
